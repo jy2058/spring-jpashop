592fb3c71a63d1c11c2d374bf456effd820dce73
jy2058 <jiy3285@gmail.com>
2023-09-10 18:47:40 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,7 +31,7 @@ public abstract class Item {
     public void removeStock(int quantity){
         int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
-            throw new ("need more stock");
+            throw new NotEnoughStockException("need more stock");
         }
         this.stockQuantity = restStock;
     }
