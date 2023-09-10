fbf0a98c6e568eb9f744d1fd3885c460c2a2ba81
jy2058 <jiy3285@gmail.com>
2023-09-10 13:45:42 +0900

    도메인 분석설계

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime date;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태 [ORDER , CANCLE]
+
+    // ==연관관계 메서드==
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItem.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
